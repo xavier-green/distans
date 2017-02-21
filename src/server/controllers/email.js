1750873582aece1188553e0b4d06a049f1121419
root <root@ip-172-31-29-227.us-west-2.compute.internal>
2017-02-21 20:12:15 +0000

    adding email settings for aws email

--- src/server/controllers/email.js
+++ src/server/controllers/email.js
@@ -1,19 +1,19 @@
 var nodemailer = require('nodemailer');
 var bluebird = require("bluebird");
 let aws = require('aws-sdk');
+aws.config.update({region:'us-west-2'});
 
 let transporter = null;
 
 if (process.env.NODE_ENV === 'production') {
 	console.log("Using AWS to send email");
-	aws.config.loadFromPath('config.json');
 	transporter = nodemailer.createTransport({
 	    SES: new aws.SES({
 	        apiVersion: 'latest'
 	    })
 	});
 } else {
-	var transporter = nodemailer.createTransport('smtps://'+process.env.GMAIL_USER.replace("@","%40")+':'+process.env.GMAIL_PASS.replace("@","%40")+'@smtp.gmail.com');
+	transporter = nodemailer.createTransport('smtps://'+process.env.GMAIL_USER.replace("@","%40")+':'+process.env.GMAIL_PASS.replace("@","%40")+'@smtp.gmail.com');
 }
 
 // setup e-mail data with unicode symbols
